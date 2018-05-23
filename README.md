b57c3c170259b94f69ddffb320eb95df0b0176c9
philliphdev <phannafo@us.ibm.com>
2018-05-23 11:35:47 -0400

    Updated logic for reset and play again button

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # hangman
-MVP
+# MVP
+ - Write a Hangman game that displays a secret word as a list of hidden letters. Also display an alphabet of letters that the user can guess while trying to guess the word. After a letter is used, disable that letter from the alphabet so the user will not try to guess the same letter twice. When a guess matches one or more letters in the secret word, reveal those letters to the user. Keep track of the number of incorrect guesses and end the game if the number of incorrect guesses reaches 6. If the user reveals all of the hidden letters before reaching 6 incorrect guesses, then the user wins.
+ ### Bonus Ideas:
+
+    -  Display a hangman as either ASCII art or images that represent the various stages of the hangman.
+     - Display a scoreboard of how games won and lost.
+      -Choose a theme for the game, such as "Famous Movies" or "Web Development Terms"
+
+
 gameboard:
   - picture of the hangman and gallow
   - instructions on how to play
--- js/script.js
+++ js/script.js
@@ -1,96 +1,106 @@
-var incorrectChoiceCounter = 6
-var letterMatch = 'no'
-const Hangman = {
-    getWordInPlay: function () {
-        const randomWord = ['PICKARD', 'KIRK', 'CRUNCH', 'MORGAN', 'SPARROW']
-        let largeNumber = Math.random()
-        let randomNumber = Math.floor(largeNumber * randomWord.length)
-        console.log("line 7 " + randomNumber)
-        const wordInPlay = randomWord[randomNumber]
-        console.log(wordInPlay)
-        this.alphabet()
-        // put underscores for letters in wordInPlay
-        let letters = $('.wordLetters')
-        letters.html = ('')
-        for (i = 0; i < wordInPlay.length; i++) {
-            let letterClass = "id = letter" + i
-            letters.append(`
-                <p ${letterClass}>_</p>
-                `)
-        }
-        this.lettersRemaining(wordInPlay)
-
-
-        return wordInPlay
-    }, // end of GetWordInPlay
-
-    updateChoicesLeft: function (num) {
-        console.log("line 28 " + num)
-        let counter = "#choicesLeftCounter"
-        $(counter).html(`
-        ${num}
-    `)
-
-        if (num === 0) {
-            console.log("line 35 " + num)
-            $('.alphabet').css('pointer-events', 'none')
-        }
-
-    },
-
-    lettersRemaining: function (word) {
-        $('.letters').click(function (event) {
-            var letterClickedOn = $(event.target).text()
-            console.log("Line 29 you clicked on " + letterClickedOn)
-
+$(document).ready(function () {
+    console.log('WE ARE BORG!')
+    var incorrectChoiceCounter = 6
+    var letterMatch = 'no'
+    const Hangman = {
+        getWordInPlay: function () {
+            const randomWord = ['PICKARD', 'KIRK', 'CRUNCH', 'MORGAN', 'SPARROW']
+            let largeNumber = Math.random()
+            let randomNumber = Math.floor(largeNumber * randomWord.length)
+            console.log("line 7 " + randomNumber)
+            const wordInPlay = randomWord[randomNumber]
+            console.log(wordInPlay)
+            this.alphabet()
+            // put underscores for letters in wordInPlay
             let letters = $('.wordLetters')
             letters.html = ('')
+            for (i = 0; i < wordInPlay.length; i++) {
+                let letterClass = "id=letter" + i
+                letters.append(`<p ${letterClass}>_</p>`)
+            }
+            this.lettersRemaining(wordInPlay)
 
-            for (i = 0; i < word.length; i++) {
-                if (letterClickedOn === word[i]) {
-                    letterMatch = "YES"
-                    let letter = "#letter" + i
-                    $(letter).html(`
-                        ${word[i]}`)
-                    console.log("hmmm line 38  " + letter)
+
+            return wordInPlay
+        }, // end of GetWordInPlay
+
+        updateChoicesLeft: function (num) {
+            console.log("line 28 " + num)
+            let counter = "#choicesLeftCounter"
+            $(counter).html(`${num}`)
+
+            if (num === 0) {
+                console.log("line 35 " + num)
+                $('.alphabet').css('pointer-events', 'none')
+            }
+
+        },
+
+        lettersRemaining: function (word) {
+            $('.letters').click(function (event) {
+                var letterClickedOn = $(event.target).text()
+                console.log("Line 29 you clicked on " + letterClickedOn)
+
+                let letters = $('.wordLetters')
+                letters.html = ('')
+
+                for (i = 0; i < word.length; i++) {
+                    if (letterClickedOn === word[i]) {
+                        letterMatch = "YES"
+                        let letter = "#letter" + i
+                        $(letter).html(`${word[i]}`)
+                        console.log("hmmm line 38  " + letter)
+                    }
+
+                    $('#' + letterClickedOn).addClass("fade")
+                    $('#' + letterClickedOn).css('pointer-events', 'none')
+                    console.log("Line 54" + letterClickedOn + i)
+                }
+                console.log("line 48 drop out of for loop " + letterMatch)
+                if (letterMatch === "YES") {
+                    letterMatch = "NO"
+                } else {
+                    incorrectChoiceCounter--
+                    letterMatch = "NO"
+                    Hangman.updateChoicesLeft(incorrectChoiceCounter)
                 }
 
-                $('#' + letterClickedOn).addClass("fade")
-                $('#' + letterClickedOn).css('pointer-events', 'none')
-                console.log("Line 54" + letterClickedOn + i)
-            }
-            console.log("line 48 drop out of for loop " + letterMatch)
-            if (letterMatch === "YES") {
-                letterMatch = "NO"
-            } else {
-                incorrectChoiceCounter--
-                letterMatch = "NO"
-                Hangman.updateChoicesLeft(incorrectChoiceCounter)
-            }
+            })
+        },  // End of lettersRemaining
 
-        })
-    },  // End of lettersRemaining
+        // put selectable letters/alphabet on screen
 
-    // put selectable letters/alphabet on screen
-
-    alphabet: function () {
-        const allLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-        let alphabetLetters = $('.alphabet')
-        alphabetLetters.html = ('')
-        for (i = 0; i < 26; i++) {
-            alphabetLetters.append(`
+        alphabet: function () {
+            const allLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+            let alphabetLetters = $('.alphabet')
+            alphabetLetters.html = ('')
+            for (i = 0; i < 26; i++) {
+                alphabetLetters.append(`
                 <p class="letters" id=${allLetters[i]}>${allLetters[i]}</p>`)
-        }
-    },  // end of alphabet
+            }
+        },  // end of alphabet
+
+        playGame: function () {
+            incorrectChoiceCounter = 6
+            Hangman.updateChoicesLeft(incorrectChoiceCounter)
+            $(".letters").remove()
+            $(".wordLetters").children().remove()
+            Hangman.getWordInPlay()
+            alert('button clicked')
+
+        }  // end playGame
 
 
 
-} // end of Hangman
+    } // end of Hangman
+    $('#start').click(Hangman.playGame)
+})
 
-window.onload = function () {
-    // // Array function to hold the correct word
-    console.log('WE ARE BORG!')
-    $('#start').click(Hangman.getWordInPlay())
+// window.onload = function () {
+//     // Array function to hold the correct word
+//     console.log('WE ARE BORG!')
+    // $('#start').click(Hangman.playGame())
+
+// }
 
 
-}
