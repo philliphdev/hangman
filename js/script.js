4e03210d15c47ab9d4938e11201a04a1c828f609
philliphdev <phannafo@us.ibm.com>
2018-05-23 20:27:24 -0400

    cleaned up logic for MVP

--- js/script.js
+++ js/script.js
@@ -9,9 +9,7 @@ $(document).ready(function () {
             const randomWord = ['PICKARD', 'KIRK', 'CRUNCH', 'MORGAN', 'SPARROW']
             let largeNumber = Math.random()
             let randomNumber = Math.floor(largeNumber * randomWord.length)
-            console.log("line 7 " + randomNumber)
             const wordInPlay = randomWord[randomNumber]
-            console.log(wordInPlay)
             this.alphabet()
             // put underscores for letters in wordInPlay
             let letters = $('.wordLetters')
@@ -27,19 +25,13 @@ $(document).ready(function () {
         }, // end of GetWordInPlay
 
         updateChoicesLeft: function (num) {
-            console.log("line 28 " + num)
             let counter = "#choicesLeftCounter"
             if (num != 99) {
                 $(counter).html(`${num}`)
                 let imageId = "#part" + (num + 1)
-                // $(imageId).toggleClass('d-none', false)
-                console.log("false " + imageId + ' ' + num)
                 Hangman.displayStatusOfImages(num + 1)
-
-
             }
             if (num === 0 | num === 99) {
-                console.log("line 35 " + num)
                 $('.alphabet').css('pointer-events', 'none')
             }
 
@@ -48,27 +40,19 @@ $(document).ready(function () {
         lettersRemaining: function (word) {
             $('.letters').click(function (event) {
                 var letterClickedOn = $(event.target).text()
-                console.log("Line 29 you clicked on " + letterClickedOn)
-
                 let letters = $('.wordLetters')
                 letters.html = ('')
-
                 for (i = 0; i < word.length; i++) {
                     if (letterClickedOn === word[i]) {
                         letterMatch = "YES"
                         correctLetters++
                         let letter = "#letter" + i
                         $(letter).html(`${word[i]}`)
-                        console.log("hmmm line 38  " + letter)
                     }
-
                     $('#' + letterClickedOn).addClass("fade")
                     $('#' + letterClickedOn).css('pointer-events', 'none')
-                    console.log("Line 54" + letterClickedOn + i)
                 }
-                console.log("line 48 drop out of for loop " + letterMatch)
                 if (word.length === correctLetters) {
-                    console.log("You win!")
                     var disableLetterClick = 99
                     Hangman.updateChoicesLeft(disableLetterClick)
                     $('.winner').toggleClass('d-none', false)
@@ -127,37 +111,31 @@ $(document).ready(function () {
                     }
                     break
                 case 6:
-                    console.log("line 127 replace")
                     $("#part6").replaceWith(`
                     <img src="img/imagepart6.png" id="part6" class="grid-item">
                     `)
                     break
                 case 5:
-                    console.log("line 127 replace")
                     $("#part2").replaceWith(`
                     <img src="img/imagepart2.png" id="part2" class="grid-item">
                     `)
                     break
                 case 4:
-                    console.log("line 127 replace")
                     $("#part5").replaceWith(`
                     <img src="img/imagepart5.png" id="part5" class="grid-item">
                     `)
                     break
                 case 3:
-                    console.log("line 127 replace")
                     $("#part1").replaceWith(`
                     <img src="img/imagepart1.png" id="part1" class="grid-item">
                     `)
                     break
                 case 2:
-                    console.log("line 127 replace")
                     $("#part4").replaceWith(`
                     <img src="img/imagepart4.png" id="part4" class="grid-item">
                     `)
                     break
                 case 1:
-                    console.log("line 127 replace")
                     $("#part3").replaceWith(`
                     <img src="img/imagepart3.png" id="part3" class="grid-item">
                     `)
@@ -173,11 +151,5 @@ $(document).ready(function () {
     $('#start').click(Hangman.playGame)
 })
 
-// window.onload = function () {
-//     // Array function to hold the correct word
-//     console.log('WE ARE BORG!')
-    // $('#start').click(Hangman.playGame())
-
-// }
 
 
